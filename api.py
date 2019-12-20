1510d05bbb486b04bc93cec1648d405683f790f1
John Bell <johnthegod@mail.ru>
2019-12-19 23:36:24 -0500

    Update api.py

--- api.py
+++ api.py
@@ -49,7 +49,7 @@ def proxFox(a):
 	proFox.set_preference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
 	proFox.update_preferences()
 	binary = FirefoxBinary(r'/usr/bin/firefox')
-        return webdriver.Firefox(capabilities=capabilities, firefox_binary=binary, options=optFox, firefox_profile=proFox)
+	return webdriver.Firefox(capabilities=capabilities, firefox_binary=binary, options=optFox, firefox_profile=proFox)
 def engine(a, b, c, d):
     try:
         if(d==True):
